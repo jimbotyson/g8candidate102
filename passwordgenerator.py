e48ccf1c83b7ae836464bb0f32378af70e3dfc15
tcouch <t.couch@ucl.ac.uk>
2021-05-14 13:33:01 +0100

    enabled setting digits, upper and lower to false

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -8,14 +8,20 @@ DIGITS = list(range(48,58)) # range counts up to, but does not include the end v
 UPPERCASE = list(range(65,91))
 LOWERCASE = list(range(97,123))
 
-def generate_password(password_length:int=10):
+def generate_password(password_length:int=10,
+                      use_digits=True,
+                      use_upper=True,
+                      use_lower=True):
     # validate the password_length
     if not isinstance(password_length, int):
         raise ValueError("password_length must be an integer value")
     elif password_length < 1:
         raise ValueError("password length cannot be less than 1")
     # create the list of characters to choose from
-    ascii_list = DIGITS + UPPERCASE + LOWERCASE
+    ascii_list = []
+    if use_digits: ascii_list += DIGITS
+    if use_upper: ascii_list += UPPERCASE
+    if use_lower: ascii_list += LOWERCASE
     ascii_list_length = len(ascii_list)
     # Randomly select character codes from ascii_list and store them in char_codes
     # Do this 'password_length' times
