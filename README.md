bd0267c2bbf6fc879e58d14bed49ee97b4c38481
tcouch <t.couch@ucl.ac.uk>
2021-05-19 09:47:22 +0100

    Using random sample and new arg names

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 A simple demonstration program in Python
 
 ## Usage
-By default generate_password generates a 10 character password containing a mix of uppercase, lowercase and digits.
+By default [passgen.py](./passgen.py) generates a 10 character password containing a mix of uppercase, lowercase and digits.
+### As a function
 ```
 >>> from passgen import generate_password
 >>> generate_password()
@@ -13,13 +14,25 @@ Pass an integer as an argument to change the password length.
 >>> generate_password(16)
 'sfp25SrTzVv5siP4'
 ```
-Set `use_digits`, `use_upper`, `use_lower` to false to remove those characters.
+Set `digits`, `uppercase`, `lowercase` to false to remove those characters.
 ```
->>> generate_password(use_lower=False)
+>>> generate_password(lowercase=False)
 'LQL4BQHZ2J'
 ```
-From the command line
+### From the command line
 ```
+$ python passgen.py --help
+usage: python3 genpw.py <args>
+
+Password generator
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -n NUMBER, --number NUMBER
+                        Number of characters
+  --nodigits            Do not include digits
+  --noupper             Do not include upper-case characters
+  --nolower             Do not include lower-case characters
 $ python passgen.py -n 8 --nolower
 TEW2SOHP
 ```
--- passgen.py
+++ passgen.py
@@ -1,67 +1,47 @@
 #!/usr/bin/env python3
 from argparse import ArgumentParser
-from random import randint
+from random import sample
+from string import digits as ascii_digits, ascii_uppercase, ascii_lowercase
 import sys
 
-# We're going to give each digit, uppercase character, and lowercase character
-# a unique number. ASCII codes already have this covered
-# See http://www.asciitable.com/
-
-DIGITS = list(range(48,58)) # range counts up to, but does not include the end value
-UPPERCASE = list(range(65,91))
-LOWERCASE = list(range(97,123))
-
 def generate_password(password_length:int=10,
-                      use_digits=True,
-                      use_upper=True,
-                      use_lower=True):
+                      digits=True,
+                      uppercase=True,
+                      lowercase=True):
     # validate the inputs
     if not isinstance(password_length, int):
         raise ValueError("password_length must be an integer value")
     elif password_length < 1:
         raise ValueError("password length cannot be less than 1")
     # don't allow all character types to be set to False
-    if not any([use_digits,use_upper,use_lower]):
+    if not any([digits,uppercase,lowercase]):
         raise ValueError("no characters available to choose from")
     # create the list of characters to choose from
-    ascii_list = []
-    if use_digits: ascii_list += DIGITS
-    if use_upper: ascii_list += UPPERCASE
-    if use_lower: ascii_list += LOWERCASE
-    ascii_list_length = len(ascii_list)
-    # Randomly select character codes from ascii_list and store them in char_codes
-    # Do this 'password_length' times
-    char_codes = []
-    for i in range(0,password_length):
-        char_code = ascii_list[randint(0,ascii_list_length-1)] # randint does include end value
-        char_codes.append(char_code)
-    # map chr() function to list of character codes to convert them
-    password_characters = map(chr,char_codes)
+    character_list = []
+    if digits: character_list += ascii_digits
+    if uppercase: character_list += ascii_uppercase
+    if lowercase: character_list += ascii_lowercase
+    # Take a random sample from character list of the requested length
+    password_characters = sample(character_list, password_length)
     # convert the list to a string and return the result
     return ''.join(password_characters)
 
 if __name__=='__main__':
     parser = ArgumentParser(
         description='Password generator',
-        usage='''python3 genpw.py [<args>]
-
--n          Number of characters
---nodigits Do not use digits
---noupper  Do not use upper-case characters
---nolower  Do not use lower-case characters
-''')
+        usage='python3 genpw.py <args>')
     parser.add_argument('-n','--number',
                         help='Number of characters')
     parser.add_argument('--nodigits', action='store_true', default=False,
                         help='Do not include digits')
     parser.add_argument('--noupper', action='store_true', default=False,
-                        help='Do not include uppercase')
+                        help='Do not include upper-case characters')
     parser.add_argument('--nolower', action='store_true', default=False,
-                        help='Do not include lowercase')
+                        help='Do not include lower-case characters')
     args = parser.parse_args(sys.argv[1:])
     kwargs = {}
     if args.number: kwargs['password_length'] = int(args.number)
-    if args.nodigits: kwargs['use_digits'] = False
-    if args.noupper: kwargs['use_upper'] = False
-    if args.nolower: kwargs['use_lower'] = False
+    if args.nodigits: kwargs['digits'] = False
+    if args.noupper: kwargs['uppercase'] = False
+    if args.nolower: kwargs['lowercase'] = False
     print(generate_password(**kwargs))
